83f19934cd3744448fb43d98dc6fd94652a8036c
anhhuydepzai <anhhuydepzai@gmail.com>
2023-02-18 20:47:32 +0700

    upload img, mp4, mp3

--- src/routes/myadmin.router.js
+++ src/routes/myadmin.router.js
@@ -2,21 +2,41 @@ const express = require('express');
 const router = express.Router();
 const myadminController = require('../app/controllers/MyAdminController.js');
 const multer  = require('multer');
-const uploadImage = multer({ dest: './src/public/media/img/' });
+var storage = multer.diskStorage(
+    {
+        destination: './src/public/media/uploads',
+        filename: function ( req, file, cb ) {
+            //req.body is empty...
+            //How could I get the new_file_name property sent from client here?
+            cb( null, file.originalname);
+        }
+    }
+);
+
+// const upload = multer({ dest: './src/public/media/uploads/' });
+var upload = multer( { storage: storage } );
 
 var authMiddleWares = require('../middlewares/auth.middlewars.js');
 
 router.get('/create', authMiddleWares.requireAuth, myadminController.showCreatePage);
 
 router.post('/stored',
-    uploadImage.single('image'),
+    upload.fields([
+        {name: 'image', maxCount: 1},
+        {name: 'mp4', maxCount: 1},
+        {name: 'mp3', maxCount: 1},
+    ]),
     authMiddleWares.requireAuth,
     myadminController.stored);
 
 router.get('/myMusic/edit/:name', authMiddleWares.requireAuth, myadminController.showEdit);
 
 router.put('/myMusic/:name',
-uploadImage.single('image'),
+    upload.fields([
+        {name: 'image', maxCount: 1},
+        {name: 'mp4', maxCount: 1},
+        {name: 'mp3', maxCount: 1},
+    ]),
 myadminController.upload);
 
 router.patch('/myMusic/restore/:id', myadminController.restore);
